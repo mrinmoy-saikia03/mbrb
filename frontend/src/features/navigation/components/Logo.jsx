e1ae39ef6f5d16673302c303f7ac086007e890c5
AbhinabhB <155467762+AbhinabhB@users.noreply.github.com>
2025-11-07 23:36:07 +0530

    production2

--- frontend/src/features/navigation/components/Logo.jsx
+++ frontend/src/features/navigation/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import MBRBLogo from "../../../assets/images/MBRBLogo.webp";
 const Logo = () => {
   return (
-    <Link href="/" className="w-48 md:w-56 lg:w-1/2 xl:w-1/3 text: 4xl" to="/">
+    <Link href="/" className="w-48 md:w-56 lg:w-1/2 xl:w-1/3 text-3xl text-white" to="/">
       {/* <img src={MBRBLogo} className="w-full h-auto" /> */}
       DreamSweets
     </Link>
